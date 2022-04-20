789df38c139d5b436a35e30aec898cb76843f21d
ItsgabrielJT <gabrielasimbaya82@gmail.com>
2022-04-19 22:07:30 -0500

    Cree metodos para cada propiedad

--- app.cpp
+++ app.cpp
@@ -9,21 +9,35 @@ public:
     {        
     }
 
-    void establecerPersonje(int _vida, string _nombre, string _poderes) {
+    Personaje &establecerPersonje(int _vida, string _nombre, string _poderes) {
+        establecerVida(_vida);
+        establecerNombre(_nombre);
+        establecerPoderes(_poderes);
+        return *this;
+    }
+
+    Personaje &establecerVida(int _vida) {
         if (typeof(_vida) == int)
             vida = _vida;
         else
             throw invalid_argument ("LA vida solo tiene que ser 100");
+        return *this;
+    }
 
+    Personaje &establecerNombre(string _nombre) {
         if ((typeof(_nombre) == string) && (_nombre.substr(25)) )
             nombre = _nombre;
         else
             throw invalid_argument ("Aqui solo se aceotan textos, o el nombre tiene que tener 25 caracteres")
+        return *this;
+    }
 
+    Personaje &establecerPoderes(string _poder) {
         if ((typeof(_poderes) == string) && (_nombre.substr(15)) )
             poderes = _poderes;
         else
             throw invalid_argument ("Aqui solo se aceotan textos, o el poder tiene que tener 15 caracteres")
+        return *this;
     }
 
 private:
